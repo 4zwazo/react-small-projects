9b1389385d113fbc4819e03a7673e68af0754b3c
4zwazo <4zwazo@gmail.com>
2020-10-11 06:39:14 -0400

    books: book data first commit

---
+++ booklist/src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/51qNkKlcbnL._AC_UL200_SR200,200_.jpg',
+    title: "Little Blue Truck's Halloween",
+    author: 'Alice Schertle',
+  },
+  {
+    id: 2,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg',
+    title: 'The Very Hungry Caterpillar',
+    author: 'Eric Carle',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/A1XyS0D1S-L._AC_UL200_SR200,200_.jpg',
+    title: 'Room on the Broom',
+    author: 'Julia Donaldson',
+  },
+]
