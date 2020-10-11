e77aac7bb9e2158e4a0f0ea3e1d8283afc867fac
4zwazo <4zwazo@gmail.com>
2020-10-11 07:00:45 -0400

    Header: initial commit

---
+++ booklist/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Amazon Best Sellers</h1>
+      <p>Our most popular products based on sales. Updated hourly.</p>
+    </div>
+  )
+}
+
+export default Header
